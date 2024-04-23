dc5cf3a2634829d0652ddf2342e1c2b2099bddac
onesmus odongo <odongo1999@gmail.com>
2024-04-23 14:46:33 +0300

    update src

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,6 @@ function About({ bio, github, linkedin }) {
       <h2>About Me</h2>
       {!bio || bio === "" ? null : <p>{bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
       <Links github={github} linkedin={linkedin}></Links>
     </div>
   );
